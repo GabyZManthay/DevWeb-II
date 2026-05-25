a287bae16e305ffe21b6e7eb076298cd3517880e
GabyZManthay <gabymanthay25@gmail.com>
2026-05-24 21:24:25 -0300

    Removendo atividade5-services antiga

--- atividade5-services/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
-import { Search } from './pages/search/search';
-
-export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'search', component: Search }
-];
